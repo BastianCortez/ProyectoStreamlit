aae4be0d86dc038abebf30f9e3f7fd987b6e3f0d
BastianCortez <bastian.cortez99@gmail.com>
2025-07-16 18:51:34 -0400

    Add files via upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# ProyectoStreamlit
-Trabajo final Streamlit
---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from pages import page1, page2, page3
+
+# Título principal
+st.set_page_config(page_title="Perfumes ColoColo", layout="wide")
+st.title("Proyecto Final de Visualización Interactiva")
+
